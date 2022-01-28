937ffb2b1b85f0a1c651c674b4240769dbd8213b
Manveer Singh <64730115+Manveer-Singh-99@users.noreply.github.com>
2022-01-28 09:33:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,39 +1,54 @@
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-var ground, gameState,engine, world,dustbin,paper;
+
+var engine, world, paper1, ground, dustbin1, dustbin2, dustbin3; 
 function setup() {
-  createCanvas(800, 400);
-  rectMode(CENTER);
+    createCanvas(800, 700);
+    rectMode(CENTER);
 
-  engine = Engine.create();
-  world = engine.world;
-  Engine.run(engine);
+    engine = Engine.create();
+    world = engine.world;
 
-  dustbin = new DustBin(720, 390, 100, 10);
-  paper = new Paper(100, 300, 10);
-  ground = Bodies.rectangle(width / 2, 400, width, 10,{isStatic: true});
-  World.add(world, ground);
+    //Create the Bodies Here.
+    paper1 = new Paper(100,600, 10);
+    ground = new Ground(400,680,800,20);
+    dustbin1 = new Dustbin(610,660, 100, 20);
+    dustbin2 = new Dustbin(550,620,20,100);
+    dustbin3 = new Dustbin(670,620,20,100)
+    
+
+    Engine.run(engine);
+  
 }
 
 function draw() {
+  
+  rectMode(CENTER);
+  
+  background(0);
+  
+  //Engine.update(engine);
+    
+  paper1.display();
 
-    background("black");
-    rectMode(CENTER);
-    background(0);
-    dustbin.display();
-    paper.display();
+  ground.display();
 
-  }
+  dustbin1.display();
+  dustbin2.display();
+  dustbin3.display();
 
+  
+}
 
 function keyPressed(){
-  if (keyPressed === UP_ARROW) {
-    Matter.Body.applyForce(paper.body, paper.body.position, {
-      x: 12,
-      y: -13
-    });
+    if (keyCode === UP_ARROW ) {
+      Matter.Body.applyForce(paper1.body, paper1.body.position, {x: 15,y: -15})
+    }
   }
-}
+
+
+
